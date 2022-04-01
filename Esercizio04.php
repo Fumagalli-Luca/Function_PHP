4e17a60f0edacdaed7aac1d6babd3890daab89c6
Fumagalli-Luca <fumagalli.luca@studenti.salesianisesto.it>
2022-04-01 18:30:06 +0200

    Final Project

--- Esercizio04.php
+++ Esercizio04.php
@@ -1,53 +1,32 @@
 <?php
 
 function CreaLista($n){
-   
+    $str="A,b,C,d,E,f,G,h,I,j,K,l,M,n,O,p,Q,r,S,t,U,v,W,x,Y,z";
+    $m=explode(",", $str);
     for($i=0;$i<$n;$i++){
-foreach($array=range("a","z",2) as $val){
-    
-$array[$i]=$val;
-}
-
+    $array[$i]=$m[rand (0,51)];
     }
     return $array;
 }
 
-
-function normale($array,$n){
-    echo "<h2> Elementi dell'array in minuscolo: </h2>";
-    for($i=0;$i<$n;$i++){
-        echo "</br>$array[$i]";
-    }
+function stampaArray($array){
+    echo "<h2> Elementi dell'array : </h2>";
+foreach($array  as $val){
+    echo "$val";
 }
-
+}
 
 function tuttoMaiuscolo($array,$n){
     echo "<h2> Elementi dell'array in maiuscolo: </h2>";
     for($i=0;$i<$n;$i++){
         $c=strtoupper($array[$i]);
-        echo "</br>$c";
+        echo "$c";
     }
-   
 }
-
-
-
-
-
-
-
-
 
 $n=10;
 $lista=CreaLista($n);
-normale($lista,$n);
+stampaArray($lista);
 tuttoMaiuscolo($lista,$n);
 
-
 ?>
-
-
-
-
-
-
